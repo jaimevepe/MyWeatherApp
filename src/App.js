3a53aa8af2df19011b5a79c718787e6a1a06b054
jaime velazquez <jaimevelazquez@Admins-MBP.attlocal.net>
2020-10-28 21:24:20 -0500

    testing if it updates with a git push

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
                     </h2>
                     <div className="city-temp">
                         {Math.round(weather.main.temp)}
-                        <sup>&deg;F</sup>
+                        <sup>&deg;Ferenheight</sup>
                     </div>
                     <div className="info">
                         <img className="city-icon" 
