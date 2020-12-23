1a7c14cb2eca3d4e6aba61981a81bc05ce0ef2b7
jaimevepe <jaime.velazquez@g.austincc.edu>
2020-12-23 14:16:36 -0600

    added comment on fetchWeather API call

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 const API_KEY = '665d0e289d7b269b701ab9d561c8f030';
-
+//export to use elsewhere
 export const fetchWeather = async (query) => {
     const { data } = await axios.get(URL, {
         params: {
